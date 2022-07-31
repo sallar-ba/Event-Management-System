4192679a12e451cb6ef994328b3f6121dadf2292
Sallar <msallar.ba@gmail.com>
2022-07-31 12:39:13 +0500

    - Added Functionality to Member Class     - Added Constructors and Functions to the Project.

--- Event-System/Member.cpp
+++ Event-System/Member.cpp
@@ -1,1 +1,33 @@
 #include "Member.h"
+Member::Member()
+{
+	this->Department = " ";
+	this->Designation = " ";
+}
+Member::Member(string Name, int Age, Date DOB, string Department, string Designation)
+{
+	setName(Name);
+	setAge(Age);
+	setDOB(DOB);
+	this->Department = Department;
+	this->Designation = Designation;
+}
+//-------------------------//
+void Member::setDepartment(string Department)
+{
+	this->Department = Department;
+}
+void Member::setDesignation(string Designation)
+{
+	this->Designation = Designation;
+}
+//------------------------//
+string Member::getDepartment() { return Department; }
+string Member::getDesignation() { return Designation; }
+//-----------------------//
+void Member::printMember()
+{
+	printPerson();
+	cout << "Department: " << this->Department << endl;
+	cout << "Designation: " << this->Designation << endl;
+}
